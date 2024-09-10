c1ddc2f69bf6ef4b5230b4de2198ecf789ea0f19
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-09-10 15:38:58 +0000

    Add serializer and modify models.py

--- gamerxchange_backend/profiles/views.py
+++ gamerxchange_backend/profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework import Response
+from .models import Profile
+from .serializers import ProfileSerializers
 
 # Create your views here.
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializers(profiles, many=True)
+        return Response(serializer.data)
