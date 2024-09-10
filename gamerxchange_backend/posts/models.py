f189617b998cf807970e89ff788ee2bd3723ecdc
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-09-10 19:39:57 +0000

    Add delete function to posts app

--- gamerxchange_backend/posts/models.py
+++ gamerxchange_backend/posts/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
-# Create your models here.
 class Post(models.Model):
-   owner = models.ForeignKey(User, on_delete=models.CASCADE)
-   title = models.CharField(max_length=100)
-   uploaded_at = models.DateTimeField(auto_now_add=True)
-   content = models.TextField(blank=True)
-   image = models.ImageField(upload_to='images/', default='../default_post_rgq6aq', blank=True)
+  
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+    
 
-class Meta:
-    ordering = ['-created_at']
-        
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return f'{self.id} {self.title}'
--- gamerxchange_backend/posts/serializers.py
+++ gamerxchange_backend/posts/serializers.py
@@ -7,7 +7,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    
+
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
@@ -24,3 +24,11 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 
+        ]
