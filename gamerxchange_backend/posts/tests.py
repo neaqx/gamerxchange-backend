335f25a0b17b22ecfba3d944e88766855d281e10
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-09-11 12:49:09 +0000

    Install comments app and modify comments.models.py

--- gamerxchange_backend/posts/tests.py
+++ gamerxchange_backend/posts/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class PostListVeiwTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='test', password='test')
+    
+    def test_create_post(self):
+        test=User.objects.get(username='test')
+        Post.objects.create(owner=test, title='Test', content='Test')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print (response.data)
+        print(len(response.data))
+        
+    def test_logged_in_user_can_create_post(self):
+        test=User.objects.get(username='test')
+        response = self.client.post('/posts/', {'title':'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        
+    def test_logged_out_user_cannot_create_post(self):
+        response = self.client.post('/posts/', {'title':'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
